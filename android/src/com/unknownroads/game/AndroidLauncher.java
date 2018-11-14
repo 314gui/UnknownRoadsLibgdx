28e865e5c4971c162652145dca0008d74a58c961
Guilherme Pereira <guilherme.pereira@xpand-it.com>
2018-11-14 15:48:02 +0000

    Initial commit

---
+++ android/src/com/unknownroads/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.unknownroads.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.unknownroads.game.UnknownRoads;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new UnknownRoads(), config);
+	}
+}
