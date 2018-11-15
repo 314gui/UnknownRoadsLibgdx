6719f6c65d6b2c1c25e0c95848f562bbae329f09
Guilherme Pereira <guilherme.pereira@xpand-it.com>
2018-11-15 11:08:50 +0000

    Loading a Map

--- core/src/com/unknownroads/game/Constants.java
+++ core/src/com/unknownroads/game/Constants.java
@@ -9,4 +9,9 @@ public class Constants {
     public static final Vector2 GRAVITY = new Vector2(0, 0);
     public static final float DEFAULT_ZOOM = 6f;
     public static final float PPM = 50.0f;
+
+    public static final String MAP_NAME = "new_map.tmx";
+
+
+
 }
