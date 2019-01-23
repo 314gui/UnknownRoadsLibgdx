ff1f50bc5c0a3c0924d45dc4b16dd1efb5b2cf97
Guilherme Pereira <guilherme.pereira@xpand-it.com>
2019-01-23 12:30:46 +0000

    tutorial changed

--- android/src/com/unknownroads/game/TutorialActivity.java
+++ android/src/com/unknownroads/game/TutorialActivity.java
@@ -1,10 +1,15 @@
 package com.unknownroads.game;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
 
 public class TutorialActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "fcul.cm.UnknownRoadsProto.EXTRA_MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +18,26 @@ public class TutorialActivity extends AppCompatActivity {
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        LinearLayout clayout = (LinearLayout) findViewById(R.id.tutoriallayout);
+        clayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Log.d("clicked", "click");
+                showMenu(v);
+
+            }
+
+        });
+    }
+
+    public void showMenu(View view) {
+
+        Intent intent = new Intent(this, MenuActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, "empty");
+        startActivity(intent);
+
     }
 }
